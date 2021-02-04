21424aea58da80336dc05e7452aeefe430e6f14f
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-04 14:40:50 -0300

    fim da aula 01.06

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,10 +1,31 @@
+const criarNovaLinha = (nome, email) => {
+    const linhaNovoCliente = document.createElement('tr')
+    
+    const conteudo =
+    `
+    <td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+        <ul class="tabela__botoes-controle">
+            <li><a href="../telas/edita_cliente.html?id=" class="botao-simples botao-simples--editar">Editar</a></li>
+            <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+        </ul>
+    </td`
+
+    linhaNovoCliente.innerHTML = conteudo;
+    return linhaNovoCliente
+};
+
+const tabela = document.querySelector('[data-tabela]')
 const http = new XMLHttpRequest();
 
 http.open('GET', 'http://localhost:3000/profile');
 
-http.send()
+http.send();
 
-http.onload = () =>{
-    const data = http.response;
-    console.log(data)
-}
+http.onload = () => {
+  const data = JSON.parse(http.response)
+  data.forEach(element => {
+      tabela.appendChild(criarNovaLinha(element.nome,element.email))
+  })
+};
