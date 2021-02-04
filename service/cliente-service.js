641fead3a68d064a8867beae4080b11affeea7e9
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-04 14:58:14 -0300

    fim da aula 01.07

--- service/cliente-service.js
+++ service/cliente-service.js
@@ -1,8 +1,7 @@
 const criarNovaLinha = (nome, email) => {
-    const linhaNovoCliente = document.createElement('tr')
-    
-    const conteudo =
-    `
+  const linhaNovoCliente = document.createElement("tr");
+
+  const conteudo = `
     <td class="td" data-td>${nome}</td>
     <td>${email}</td>
     <td>
@@ -10,22 +9,36 @@ const criarNovaLinha = (nome, email) => {
             <li><a href="../telas/edita_cliente.html?id=" class="botao-simples botao-simples--editar">Editar</a></li>
             <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
         </ul>
-    </td`
+    </td`;
 
-    linhaNovoCliente.innerHTML = conteudo;
-    return linhaNovoCliente
+  linhaNovoCliente.innerHTML = conteudo;
+  return linhaNovoCliente;
 };
 
-const tabela = document.querySelector('[data-tabela]')
-const http = new XMLHttpRequest();
+const tabela = document.querySelector("[data-tabela]");
 
-http.open('GET', 'http://localhost:3000/profile');
+const listaClientes = () => {
+  const promise = new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
 
-http.send();
+    http.open("GET", "http://localhost:3000/profile");
 
-http.onload = () => {
-  const data = JSON.parse(http.response)
-  data.forEach(element => {
-      tabela.appendChild(criarNovaLinha(element.nome,element.email))
-  })
+    http.send();
+
+    http.onload = () => {
+      if (http.status >= 400) {
+        reject(JSON.parse(http.response));
+      } else {
+        resolve(JSON.parse(http.response));
+      }
+    };
+  });
+  return promise;
 };
+
+listaClientes()
+.then(data =>{        
+        data.forEach(element => {
+          tabela.appendChild(criarNovaLinha(element.nome, element.email));
+})})
+
